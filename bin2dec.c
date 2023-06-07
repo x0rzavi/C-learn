4377bbaeb7e6d339010ce8626eeb6866e981d993
Avishek Sen <x0rzavi@gmail.com>
2023-06-07 09:48:08 +0530

    Wed Jun  7 09:48:08 AM IST 2023

---
+++ bin2dec.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+        int n;
+        printf("Enter binary: ");
+        scanf("%d", &n);
+
+        int power = 0, sum = 0, digit;
+        while (n != 0) {
+                digit = n % 10;
+                sum = sum + (digit * pow(2, power));
+                n = n / 10;
+                power++;
+        }
+
+        printf("Decimal number: %d", sum);
+        return 0;
+}
---
+++ powRec.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int powRec(int a, int b);
+
+int main() {
+        int a, b;
+        printf("Enter a, b: ");
+        scanf("%d%d", &a, &b);
+
+        printf("a ^ b: %d", powRec(a, b));
+        return 0;
+}
+
+int powRec(int a, int b) {
+        if (b == 0) {
+                return 1;
+        } else {
+                return a * powRec(a, b - 1);
+        }
+}

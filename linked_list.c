e27b9daf35a363e33019b7d036f12c8d9cf048b5
Avishek Sen <x0rzavi@gmail.com>
2023-10-30 21:10:59 +0530

    Mon Oct 30 09:10:59 PM IST 2023

--- linked_list.c
+++ linked_list.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 typedef struct node {
         int data;
         struct node *next;
 } node;
-node *start = NULL, *new_node, *temp = NULL, *prev = NULL;
+node *start = NULL, *new_node = NULL, *temp = NULL, *prev = NULL, *next = NULL;
 
 void create();
 void insert_begn();
@@ -23,30 +24,35 @@ int count();
 int main() {
         printf(" 1. Insert at Beginning\n");
         printf(" 2. Insert at End\n");
-        printf(" 3. Delete from Beginning\n");
-        printf(" 4. Delete from End\n");
-        printf(" 5. Insert before Node\n");
-        printf(" 6. Insert after Node\n");
-        printf(" 7. Insert at Position\n");
+        printf(" 3. Insert before Node\n");
+        printf(" 4. Insert after Node\n");
+        printf(" 5. Insert at Position\n");
+        printf(" 6. Delete from Beginning\n");
+        printf(" 7. Delete from End\n");
         printf(" 8. Delete before Node\n");
         printf(" 9. Delete after Node\n");
         printf("10. Delete at Position\n");
-        printf("11. Display\n");
-        printf("12. Count Items\n");
-        printf("13. Exit\n");
+        printf("11. Reverse\n");
+        printf("12. Sort\n");
+        printf("13. Search\n");
+        printf("14. Count\n");
+        printf("15. Display\n");
+        printf("16. Exit\n");
         while (1) {
                 int c; printf("\nEnter choice: "); scanf("%d", &c);
                 switch (c) {
                         case 1: insert_begn(); break;
                         case 2: insert_end(); break;
-                        case 3: delete_begn(); break;
-                        case 4: delete_end(); break;
-                        case 5: insert_bef(); break;
-                        case 6: insert_aft(); break;
-                        case 7: insert_pos(); break;
-                        case 11: display(); break;
-                        case 12: count(); break;
-                        case 13: exit(1);
+                        case 3: insert_bef(); break;
+                        case 4: insert_aft(); break;
+                        case 5: insert_pos(); break;
+                        case 6: delete_begn(); break;
+                        case 7: delete_end(); break;
+                        case 8: delete_bef(); break;
+                        case 9: delete_aft(); break;
+                        case 14: count(); break;
+                        case 15: display(); break;
+                        case 16: exit(1);
                         default: printf("Wrong choice!\n");
                 }
         }
@@ -114,7 +120,7 @@ void delete_begn() {
                 printf("Underflow!");
         } else {
                 temp = start;
-                printf("Deleted: %d", temp->data);
+                printf("Deleted: %d\n", temp->data);
                 start = temp->next;
         }
         printf("\n");
@@ -125,7 +131,7 @@ void delete_end() {
                 printf("Underflow!");
         } else {
                 if (start->next == NULL) { // check if 1st node
-                        printf("Deleted: %d", start->data);
+                        printf("Deleted: %d\n", start->data);
                         start = NULL;
                 } else {
                         temp = start;
@@ -133,7 +139,7 @@ void delete_end() {
                                 prev = temp;
                                 temp = temp->next;
                         }
-                        printf("Deleted: %d", temp->data);
+                        printf("Deleted: %d\n", temp->data);
                         prev->next = NULL;
                 }
         }
@@ -156,7 +162,6 @@ void insert_bef() {
                 new_node->next = prev->next;
                 prev->next = new_node;
         }
-
 }
 
 void insert_aft() {
@@ -184,14 +189,49 @@ void insert_pos() {
         } else if (pos > 1 && pos < c) {
                 int i = 1;
                 temp = start;
-                while (temp != NULL && i < pos) { // iterating pos times
+                while (temp != NULL && i < pos - 1) { // iterating upto pos - 1 times; just before node to insert before
                         temp = temp->next;
                         i++;
                 }
                 create();
-                new_node->next = temp->next; // inserting after node
+                new_node->next = temp->next; // inserting before node
                 temp->next = new_node;
         } else {
                 printf("Invalid postion!\n");
         }
+}
+
+void delete_bef() {
+        int data; printf("Enter data item to delete before: "); scanf("%d", &data);
+        temp = start;
+        while (temp != NULL && temp->next->data != data) {
+                prev = temp;
+                temp = temp->next;
+        }
+        if (temp == NULL) {
+                printf("Data item %d not found!\n", data);
+        } else if (prev == start) { // if prev is not defined delete from beginning
+                delete_begn();
+        } else {
+                printf("Deleted: %d\n", temp->data);
+                prev->next = temp->next;
+                free(temp);
+        }
+}
+
+void delete_aft() {
+        int data; printf("Enter data item to delete after: "); scanf("%d", &data);
+        temp = start;
+        while (temp != NULL && temp->data != data) {
+                temp = temp->next;
+        }
+        if (temp == NULL) {
+                printf("Data item %d not found!\n", data);
+        } else {
+                next = temp->next;
+                printf("Deleted: %d\n", next->data);
+                next->next = temp->next;
+                free(temp);
+        }
+
 }
